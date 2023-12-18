357323c378e01d064bc0bf6020416537b4c71358
makeitvsolo <vane4ka.dev@gmail.com>
2023-12-18 08:42:57 +0300

    :runner: feat: Unsafe annotation in `:core`

---
+++ rest-api/internal/core/src/main/java/github/makeitvsolo/fstored/core/unsafe/Unsafe.java
@@ -0,0 +1,13 @@
+package github.makeitvsolo.fstored.core.unsafe;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+public @interface Unsafe {
+
+    String safety();
+}
