f93dbfd8610d0ba0079ba0daaa088bdab4da7452
makeitvsolo <vane4ka.dev@gmail.com>
2023-12-19 04:21:26 +0300

    :runner: feat(#1): impl MoveFileUsecase in `:storage:application`

--- rest-api/internal/storage/application/src/main/java/github/makeitvsolo/fstored/storage/application/storage/FileStorage.java
+++ rest-api/internal/storage/application/src/main/java/github/makeitvsolo/fstored/storage/application/storage/FileStorage.java
@@ -6,6 +6,7 @@ import github.makeitvsolo.fstored.storage.application.storage.source.BinarySourc
 public interface FileStorage<H extends FileHandle> {
 
     void write(H handle, BinarySource source);
+    void move(H source, H destination);
 
     boolean exists(H handle);
 }
