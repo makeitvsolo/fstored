d8a09b61abd62650971cbb6405b9227fa2eb9157
makeitvsolo <vane4ka.dev@gmail.com>
2023-12-19 05:59:31 +0300

    :runner: feat(#1): impl RemoveFolderUsecase in `:storage:application`

--- rest-api/internal/storage/application/src/main/java/github/makeitvsolo/fstored/storage/application/storage/FolderStorage.java
+++ rest-api/internal/storage/application/src/main/java/github/makeitvsolo/fstored/storage/application/storage/FolderStorage.java
@@ -6,6 +6,7 @@ public interface FolderStorage<H extends FolderHandle> {
 
     void make(H handle);
     void move(H source, H destination);
+    void remove(H handle);
 
     boolean exists(H handle);
 }
