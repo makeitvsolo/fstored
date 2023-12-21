bb2ba112ffc166ed3ad519d417556b816deea738
makeitvsolo <vane4ka.dev@gmail.com>
2023-12-21 05:34:37 +0300

    :runner: feat(#3): FstoredApplication now bootable in `:boot`

---
+++ rest-api/boot/src/main/java/github/makeitvsolo/fstored/boot/FstoredApplication.java
@@ -0,0 +1,12 @@
+package github.makeitvsolo.fstored.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FstoredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FstoredApplication.class, args);
+    }
+}
