30b750b1740a01c327d298fa5ca7885152665440
makeitvsolo <vane4ka.dev@gmail.com>
2023-12-21 05:55:12 +0300

    :runner: feat(#3): impl FileUsecasesConfiguration in `:boot`

---
+++ rest-api/boot/src/main/java/github/makeitvsolo/fstored/boot/config/internal/storage/usecase/FileUsecasesConfiguration.java
@@ -0,0 +1,69 @@
+package github.makeitvsolo.fstored.boot.config.internal.storage.usecase;
+
+import github.makeitvsolo.fstored.storage.application.mapping.IntoFileContentDtoMapper;
+import github.makeitvsolo.fstored.storage.application.storage.FileStorage;
+import github.makeitvsolo.fstored.storage.application.storage.handle.ComposeFileHandle;
+import github.makeitvsolo.fstored.storage.application.usecase.file.*;
+import github.makeitvsolo.fstored.storage.minio.handle.MinioHandle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FileUsecasesConfiguration {
+
+    @Bean
+    public FetchFileUsecase<MinioHandle> fetchFileUsecase(
+            final FileStorage<MinioHandle> fileStorage,
+            final ComposeFileHandle<MinioHandle> composeFileHandle
+    ) {
+        return new FetchFileUsecase<>(
+                fileStorage,
+                composeFileHandle,
+                new IntoFileContentDtoMapper()
+        );
+    }
+
+    @Bean
+    public MakeFileUsecase<MinioHandle> makeFileUsecase(
+            final FileStorage<MinioHandle> fileStorage,
+            final ComposeFileHandle<MinioHandle> composeFileHandle
+    ) {
+        return new MakeFileUsecase<>(
+                fileStorage,
+                composeFileHandle
+        );
+    }
+
+    @Bean
+    public MoveFileUsecase<MinioHandle> moveFileUsecase(
+            final FileStorage<MinioHandle> fileStorage,
+            final ComposeFileHandle<MinioHandle> composeFileHandle
+    ) {
+        return new MoveFileUsecase<>(
+                fileStorage,
+                composeFileHandle
+        );
+    }
+
+    @Bean
+    public RemoveFileUsecase<MinioHandle> removeFileUsecase(
+            final FileStorage<MinioHandle> fileStorage,
+            final ComposeFileHandle<MinioHandle> composeFileHandle
+    ) {
+        return new RemoveFileUsecase<>(
+                fileStorage,
+                composeFileHandle
+        );
+    }
+
+    @Bean
+    public WriteFileUsecase<MinioHandle> writeFileUsecase(
+            final FileStorage<MinioHandle> fileStorage,
+            final ComposeFileHandle<MinioHandle> composeFileHandle
+    ) {
+        return new WriteFileUsecase<>(
+                fileStorage,
+                composeFileHandle
+        );
+    }
+}
