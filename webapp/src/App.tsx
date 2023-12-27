fd346d8a902b19f3c1759803c39bffcf4dc656d0
makeitvsolo <vane4ka.dev@gmail.com>
2023-12-27 12:13:38 +0300

    :runner: feat(#4): impl Home in `@pages`

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Layout } from "@pages";
+import { Home, Layout } from "@pages";
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
