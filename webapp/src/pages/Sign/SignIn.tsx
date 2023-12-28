a360a2c33383bc0fc0aa3cabd515e8e20133b909
makeitvsolo <vane4ka.dev@gmail.com>
2023-12-28 10:54:28 +0300

    :runner: feat(#4): impl SignInForm in `@pages`

--- webapp/src/pages/Sign/SignIn.tsx
+++ webapp/src/pages/Sign/SignIn.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Box,
   Button,
@@ -8,25 +10,70 @@ import {
   Stack,
   VStack,
   Text,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
 } from "@chakra-ui/react";
+
+import { useSignIn, Message } from "@service";
 
 export interface SignInProps {
   onSwitch: () => void;
 }
 
 const SignInForm = () => {
+  const { loading, execute } = useSignIn();
+
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({ name: "", password: "" });
+  const [message, setMessage] = useState<Message>({} as Message);
+
+  const onSubmit = async () => {
+    const msg = await execute(credentials.name, credentials.password);
+
+    if (msg.ok) {
+      navigate("/");
+    }
+
+    setMessage(msg);
+  };
+
   return (
     <Box minW="container.sm" borderWidth={1} borderRadius={8} boxShadow="lg">
       <Stack p={12} spacing={4}>
+        {message.error && (
+          <Alert status="error">
+            <AlertIcon />
+            <AlertTitle>error:</AlertTitle>
+            <AlertDescription>{message.error}</AlertDescription>
+          </Alert>
+        )}
         <FormControl isRequired>
           <FormLabel>Name</FormLabel>
-          <Input aria-label="username" />
+          <Input
+            aria-label="username"
+            onChange={(e) =>
+              setCredentials({ ...credentials, name: e.target.value })
+            }
+          />
         </FormControl>
         <FormControl isRequired>
           <FormLabel>Password</FormLabel>
-          <Input aria-label="password" type="password" />
+          <Input
+            aria-label="password"
+            type="password"
+            onChange={(e) =>
+              setCredentials({ ...credentials, password: e.target.value })
+            }
+          />
         </FormControl>
-        <Button colorScheme="blue" variant="outline">
+        <Button
+          colorScheme="blue"
+          variant="outline"
+          isLoading={loading.current}
+          onClick={onSubmit}
+        >
           Sign in
         </Button>
       </Stack>
