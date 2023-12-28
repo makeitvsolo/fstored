6fda52233abbad86c6662a9aee8d165131ff693f
makeitvsolo <vane4ka.dev@gmail.com>
2023-12-28 10:54:18 +0300

    :runner: feat(#4): impl useSignIn hook in `@service`

--- webapp/src/service/users.ts
+++ webapp/src/service/users.ts
@@ -1,5 +1,6 @@
 import { Err, usersApi } from "@api";
 import { useRequest } from "@service";
+import { useUserStore } from "@store";
 
 export interface Message {
   ok: string | null;
@@ -14,6 +15,40 @@ export const useSignUp = () => {
       await execute(name, password);
       return {
         ok: "user created",
+        error: null,
+      } as Message;
+    } catch (err) {
+      if ((err as Err).details) {
+        return {
+          ok: null,
+          error: (err as Err).details,
+        } as Message;
+      }
+
+      return {
+        ok: null,
+        error: "unexpected error",
+      } as Message;
+    }
+  };
+
+  return {
+    loading: loading,
+    execute: proxy,
+  };
+};
+
+export const useSignIn = () => {
+  const { loading, execute } = useRequest(usersApi.signIn);
+  const setUser = useUserStore((state) => state.setActiveUser);
+
+  const proxy = async (name: string, password: string): Promise<Message> => {
+    try {
+      const token = await execute(name, password);
+      setUser(token.active);
+
+      return {
+        ok: "user signed",
         error: null,
       } as Message;
     } catch (err) {
