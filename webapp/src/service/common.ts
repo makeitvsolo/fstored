c216a71dce7f003ef6058487e49a1a82d5fc1c84
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-01 18:35:40 +0300

    :runner: feat(#4): impl useQuery and useMutation in `@service`

---
+++ webapp/src/service/common.ts
@@ -0,0 +1,68 @@
+import { useRef } from "react";
+
+import { Err } from "@api";
+import { useUserStore } from "@store";
+
+export const useQuery = <QueryFn extends (...args: any[]) => any>(
+  fn: QueryFn
+) => {
+  const loading = useRef(false);
+  const data = useRef<Awaited<ReturnType<typeof fn>>["data"] | null>(null);
+
+  const removeUser = useUserStore((state) => state.removeActiveUser);
+
+  const proxy = async (...args: Parameters<QueryFn>) => {
+    try {
+      loading.current = true;
+
+      const response = await fn(args);
+      data.current = response;
+
+      loading.current = false;
+    } catch (err) {
+      if ((err as Err).status_code === 401) {
+        removeUser();
+      }
+
+      loading.current = false;
+      throw err;
+    }
+  };
+
+  return {
+    loading,
+    data,
+    refetch: proxy,
+  };
+};
+
+export const useMutation = <MutationFn extends (...args: any[]) => any>(
+  fn: MutationFn
+) => {
+  const loading = useRef(false);
+
+  const removeUser = useUserStore((state) => state.removeActiveUser);
+
+  const proxy = async (...args: Parameters<MutationFn>) => {
+    try {
+      loading.current = true;
+
+      const response = await fn(args);
+
+      loading.current = false;
+      return response;
+    } catch (err) {
+      if ((err as Err).status_code === 401) {
+        removeUser();
+      }
+
+      loading.current = false;
+      throw err;
+    }
+  };
+
+  return {
+    loading,
+    execute: proxy,
+  };
+};
