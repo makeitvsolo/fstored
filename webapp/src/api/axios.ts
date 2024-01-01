18d33fb6a65854091e230eb7b6dc8ae9188bfeac
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-01 18:14:01 +0300

    :runner: feat(#4): add axios instance in `@api`

---
+++ webapp/src/api/axios.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export interface Ok<T> {
+  status_code: number;
+  status: string;
+  data: T;
+}
+
+export interface Err {
+  status_code: number;
+  error: string;
+  details: string;
+}
+
+export const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (ok: AxiosResponse) => {
+    if (ok.data.data) {
+      return ok.data.data;
+    }
+
+    return {};
+  },
+
+  (err: AxiosError<Err> | Error) => {
+    if (axios.isAxiosError<Err>(err) && err.response) {
+      throw err.response.data;
+    }
+
+    console.log(err);
+    throw err;
+  }
+);
