4e0e955517ff369f686deab61920d2338a4a03e4
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-02 10:19:13 +0300

    :runner: feat(#4): impl Sign in `@pages`

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Empty, Home, Layout } from "@pages";
+import { Empty, Home, Layout, Sign } from "@pages";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="sign" element={<Sign />} />
         <Route path="*" element={<Empty />} />
       </Route>
     </Routes>
